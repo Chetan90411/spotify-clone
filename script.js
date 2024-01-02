3d2b1ddf3139ff04c5730a5ef0b1f64850f3049e
Chetan90411 <chetan738090@gmail.com>
2024-01-02 16:47:05 +0530

    Playbar changes

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let currentSong = new Audio();
+
+function secondsToMinutesAndSeconds(seconds) {
+  minutes = parseInt(seconds / 60);
+  seconds = parseInt(seconds % 60);
+
+  return `${(minutes > 9) ? minutes : "0" + minutes}:${(seconds > 9) ? seconds : "0" + seconds}`;
+}
+
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/");
   let response = await a.text();
@@ -14,17 +23,33 @@ async function getSongs() {
   return songs;
 }
 
+const playMusic = (track, pause = "false") => {
+  // let audio = new Audio("/songs/" + track);
+  currentSong.src = "/songs/" + track;
+  if(!pause) {
+    currentSong.play();
+    play.src = "pause.svg";
+  }
+  document.querySelector(".songinfo").innerHTML = track;
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+};
+
 async function main() {
   // Get List of all the Songs
   let songs = await getSongs();
-  console.log(songs);
+  playMusic(songs[0], true);
+
   // Show all the songs in the playlist
-  let songUL = document.querySelector(".songList").getElementsByTagName('ul')[0]
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
   for (const song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `<li>
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li>
               <img class="invert" src="music.svg" alt="">
               <div class="info">
-                <div>${song.replaceAll('%20', ' ')}</div>
+                <div>${song.replaceAll("%20", " ")}</div>
                 <div>Chetan</div>
               </div>
               <div class="playnow">
@@ -34,6 +59,32 @@ async function main() {
     </li>`;
   }
 
+  // Attach an event Listener to each song
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
+
+  // Attach event listener to play, next and previous
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "pause.svg";
+    } else {
+      currentSong.pause();
+      play.src = "play.svg";
+    }
+  });
+
+  // Listen for timeupdate Event
+  currentSong.addEventListener('timeupdate', () => {
+    console.log(currentSong.currentTime, secondsToMinutesAndSeconds(currentSong.duration))
+    document.querySelector('.songtime').innerHTML = `${secondsToMinutesAndSeconds(currentSong.currentTime)}/${secondsToMinutesAndSeconds(currentSong.duration)}`
+  })
 }
 
 main();
