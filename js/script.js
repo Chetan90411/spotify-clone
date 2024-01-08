29ff9fc8e8c4506ae1ec1117f73ced4a2603cc44
Chetan90411 <chetan738090@gmail.com>
2024-01-08 16:28:02 +0530

    path change

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function secondsToMinutesAndSeconds(seconds) {
 
 async function getSongs(folder) {
   curfolder = folder;
-  let a = await fetch(`http://127.0.0.1:3000/${folder}/`);
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -75,7 +75,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-  let a = await fetch(`http://127.0.0.1:3000/songs/`);
+  let a = await fetch(`/songs/`);
   let response = await a.text();
 
   let div = document.createElement("div");
@@ -89,7 +89,7 @@ async function displayAlbums() {
     if (e.href.includes("/songs")) {
       let folder = e.href.split("/").slice(-2)[0];
       // Get metadata of the folder
-      let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+      let a = await fetch(`/songs/${folder}/info.json`);
       let response = await a.json();
       let cardContainer = document.querySelector(".cardContainer");
       cardContainer.innerHTML =
@@ -116,7 +116,7 @@ async function displayAlbums() {
   Array.from(document.getElementsByClassName("card")).forEach((e) => {
     e.addEventListener("click", async (item) => {
       songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
-      playMusic(songs[0])
+      playMusic(songs[0]);
     });
   });
 }
